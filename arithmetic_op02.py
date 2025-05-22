66afa45b77920b596718ee4dcfd2ccf298f89cdf
Xushnudd <boshhacker1@gmail.com>
2025-05-22 16:39:56 +0500

    bajarildi

--- arithmetic_op02.py
+++ arithmetic_op02.py
@@ -1,5 +1,8 @@
 #Create a varaible 'number' and assign it the value of 43.
-
+number = 43
 #Create a variable 'answer' and assign it the sum of the number's digits.
-
+x = number // 10
+y = number % 10
+answer = x + y
 #Print the vallue of the 'answer'.
+print(answer)
--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -1,7 +1,10 @@
 #Create a variable called 'number' and assign it the two-digit number
-
+number = 45
 #Find the reverse of the number and assign it to a variable called 'answer'.
-
+x = number // 10
+y = number % 10
+answer = f"{y}{x}"
 #Print the value of the 'answer'
+print(answer)
 
 
